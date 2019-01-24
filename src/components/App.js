cc1b147866c64fd39e05261f0ef7db3d6157d5e9
Jamie Pham <jtkp66@gmail.com>
2019-01-24 12:51:40 -0800

    Added helper methods to gather input data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
+}
 
 export default App;
+
+// The onSubmit property on line 12 is not the same as the 
+// onSubmit property of a normal jsx element in SearchBar.js file
+// Passing in properties and callbacks we can call it whatever we like
+// but it's better to name it something close to it's purpose. 
+// onSubmit here is to be able to pass properties with a trick to let App
+// pass down props to SearchBar
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
     state = { term: '' };
 
+    // Assign an arrow function rather than an Object to function to bind
+    // Can also bind this to onSubmit in render
+    // <form onSubmit={(e) => this.onFormSubmit(e)} className="ui form">
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
